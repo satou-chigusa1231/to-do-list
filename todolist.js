45ed211cf8e5d6ea10a7721c0aaa71c468897c0e
satou-chigusa1231 <satou.chigusa1231@gmail.com>
2020-05-18 18:43:47 +0900

    first commit

---
+++ todolist.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Register = document.getElementsByClassName('register')[0];
+const addlist = document.getElementsByClassName('addlist')[0];
+const Inputfield = document.getElementsByClassName('inputfield')[0];
+
+const removeTask = removeButtona => {
+    const targetTask = removeButtona.closest('li');
+    addlist.removeChild(targetTask);
+};
+
+const addTask = taskb => {
+ const listItem = document.createElement('li');
+ const removeButton = document.createElement('button');
+
+ removeButton.innerText = 'Delete';
+ removeButton.addEventListener('click' , ()=> removeTask(removeButton));
+
+ listItem.innerText = taskb;
+ addlist.appendChild(listItem);
+listItem.appendChild(removeButton);
+ 
+};
+
+Register.addEventListener('click', () => {
+    const task = Inputfield.value;
+    Inputfield.value='';
+    addTask(task);
+});
